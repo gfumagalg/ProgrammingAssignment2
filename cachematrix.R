794776349fd5db707ee251c20ec6a2b77915655d
Gerardo Fumagal <gfumagalg@tec.mx>
2020-07-30 18:56:47 -0500

    Two functions code to define a vector of setters and getters operators, solve the inverse of a matrix and cache it

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,37 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix creates a list with setter and getter functions to
+## set and get the values of a matrix, set and get the inverse of that matrix
 
+makeCacheMatrix <- function(x = matrix()) {
+    invM <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverseM <- function(inverseM) invM <<- inverseM
+    getinverseM <- function() invM
+    list(set = set, get = get,
+        setinverseM = setinverseM,
+        getinverseM = getinverseM)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+    invM <- x$getinverse()
+    if(!is.null(invM)) {
+      message("getting cached data")
+      return(invM)
+    }
+    data <- x$get()
+    invM <- solve(data, ...)
+    x$setinverse(invM)
+    invM
+  
         ## Return a matrix that is the inverse of 'x'
 }
